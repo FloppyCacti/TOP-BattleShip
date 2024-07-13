8c82376212972827e2186fae064057a81aa8a6c3
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-07-13 12:56:20 -0500

    revert gameboard tests

--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -18,6 +18,20 @@ it("place ship at specific coordinates", () => {
   expect(gameBoard.board[5][4]).toBe(testShip);
 });
 
-it("check if cordinates are invalid", () => {
+it("check if cordinates are out of range", () => {
   expect(gameBoard.place(testShip, [11, 11], true)).toBe("Outside of range");
+});
+
+it("check if cordinates are hit", () => {
+  expect(gameBoard.receiveAttack([3, 3])).toEqual([3, 3]);
+});
+
+it("check if receiveAttack records hit", () => {
+  gameBoard.place(testShip, [5, 4], true);
+  gameBoard.receiveAttack([5, 4]);
+  expect(testShip.hits).toBe(1);
+});
+
+it("check if receiveAttack records misses", () => {
+  expect(gameBoard.receiveAttack([1, 2])).toBe([1, 2]);
 });
