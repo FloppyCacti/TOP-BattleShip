6448d49511279787e7cd25fea3fc919d1f72a16a
Parth Patel <parthvapi2005@gmail.com>
2024-07-12 22:25:47 -0500

    add deleted test code back

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -1,20 +1,37 @@
+const Gameboard = require("../gameboard");
 const Ship = require("../ship");
 
-it("inital ship setup", () => {
-  const testShip = new Ship(4);
-  expect(testShip.length).toBe(4);
-  expect(testShip.hits).toBe(0);
-  expect(testShip.sunk).toBe(false);
+let gameBoard;
+let testShip;
+
+beforeEach(() => {
+  gameBoard = new Gameboard();
+  testShip = new Ship(1);
 });
 
-it("function to increase ship hits", () => {
-  const testShip = new Ship(4, 0);
-  testShip.hit();
+it("check gameboard size", () => {
+  expect(gameBoard.board.length).toBe(10);
+});
+
+it("place ship at specific coordinates", () => {
+  gameBoard.place(testShip, [5, 4], true);
+  expect(gameBoard.board[5][4]).toBe(testShip);
+});
+
+it("check if cordinates are out of range", () => {
+  expect(gameBoard.place(testShip, [11, 11], true)).toBe("Outside of range");
+});
+
+it("check if cordinates are hit", () => {
+  expect(gameBoard.receiveAttack([3, 3])).toEqual([3, 3]);
+});
+
+it("check if receiveAttack records hit", () => {
+  gameBoard.place(testShip, [5, 4], true);
+  gameBoard.receiveAttack([5, 4]);
   expect(testShip.hits).toBe(1);
 });
 
-it("function to check if ship is sunk", () => {
-  const testShip = new Ship(4, 4, false);
-  testShip.isSunk();
-  expect(testShip.sunk).toBe(true);
+it("check if receiveAttack records misses", () => {
+  expect(gameBoard.receiveAttack([1, 2])).toBe([1, 2]);
 });
